4597a609d548ce001795590cb1997f15bc83fb7a
LeoAlyabiew <leo.alyabiew88@gmail.com>
2023-12-04 19:45:24 +0300

    Module-2

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,4 +3,7 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.home),
+    path('countries-list/', views.countries_list, name='countries_list'),
+    path('countries-list/<str:country_name>/', views.countries_list_detail, name='detail'),
+    path('languages-list', views.languages_list, name='languages_list'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,45 @@
+import json
 from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def countries_list(request):
+    with open('./country-by-languages.json', 'r') as countries:
+        data = json.load(countries)
+        result = []
+        for item in data:
+            result.append(item['country'])
+
+    return render(request, 'countries-list.html', {'countries': result})
+
+
+def countries_list_detail(request, country_name):
+    with open('./country-by-languages.json', 'r') as countries:
+        data = json.load(countries)
+        result = []
+        for item in data:
+            if item['country'] == country_name:
+                result = item['languages']
+                break
+
+    return render(request, 'countries-list-detail.html', {
+        'country_name': country_name,
+        'languages': result,
+    })
+
+
+def languages_list(request):
+    with open('./country-by-languages.json', 'r') as countries:
+        data = json.load(countries)
+        result = []
+        for item in data:
+            result += item['languages']
+        _result = set(result)
+        result = list(_result)
+        result = sorted(result)
+
+    return render(request, 'languages-list.html', {'languages': result})
