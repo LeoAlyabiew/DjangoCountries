697910ceb7745853913bf9458cbccd163bdf4025
LeoAlyabiew <leo.alyabiew88@gmail.com>
2024-01-12 20:27:12 +0300

    Module-7

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,8 +3,9 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.home),
-    path('countries-list/', views.countries_list, name='countries_list'),
+    path('countries-list/<int:num_page>/', views.countries_list, name='countries_list'),
     path('countries-list/<str:country_name>/', views.countries_list_detail, name='country_detail'),
+    path('sorting/<str:letter>/', views.sorting, name='sorting'),
     path('languages-list', views.languages_list, name='languages_list'),
     path('languages-list/<str:language_name>', views.languages_list_detail, name='language_detail'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,18 +1,29 @@
 import json
+
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 from MainApp.models import Country, Language
 
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 
-def countries_list(request):
+def countries_list(request, num_page):
     countries = Country.objects.all()
+    paginator = Paginator(countries, 10)
+    page = paginator.page(num_page)
+    countries_list = page.object_list
 
-    return render(request, 'countries-list.html', {'countries': countries})
+    return render(request, 'countries-list.html', {
+        'countries': countries_list,
+        'paginator': paginator,
+        'alphabet': ALPHABET,
+    })
 
 
 def countries_list_detail(request, country_name):
@@ -38,4 +49,13 @@ def languages_list_detail(request, language_name):
     return render(request, 'languages-list-detail.html', {
         'countries': countries,
         'language': language_name,
+    })
+
+
+def sorting(request, letter):
+    countries = Country.objects.filter(name__startswith=letter)
+
+    return render(request, 'countries-list.html', {
+        'countries': countries,
+        'alphabet': ALPHABET,
     })
